6a63f6b0f4cb4c94acc339d6946df8d609f43784
alanacanete <167706341+alanacanete@users.noreply.github.com>
2024-11-16 23:26:36 -0300

    Delete tpfinalparte1 directory

--- tpfinalparte1/processing_exe/botones_presionados.js
+++
@@ -1,5 +0,0 @@
-//function ZonaBoton (valor,limX1,limX2,limY1,limY2, nuevoestado){
-//  if (estado===valor && mouseX >= limX1 && mouseX <= limX2 && mouseY >= limY1 && mouseY <= limY2) 
-//      estado=nuevoestado;
-//}
-//}
